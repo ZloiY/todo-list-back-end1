f476c97b9a84f2d992378fa2857e9e4434b1c023
ZloiY <NoRFolf1000@gmail.com>
2017-07-04 15:33:03 +0300

    feat: add get methods

--- app/src/jwt/jwt-creating.js
+++ app/src/jwt/jwt-creating.js
@@ -2,8 +2,23 @@ const idGen = require('../generators/id-gen');
 const secretGen = require('../generators/salt-gen');
 const jwt = require('jsonwebtoken');
 
+let secret;
+let ident;
+
 exports.getToken = function () {
-  const id = { id: idGen.idGen(), iat: Math.floor(Date.now() / 1000) + (60 * 60),};
-  const secret = secretGen.saltGen();
+  ident = idGen.idGen();
+  const id = {
+    id: ident,
+    iat: Math.floor(Date.now() / 1000),
+  };
+  secret = secretGen.saltGen();
   return jwt.sign(id, secret);
+};
+
+exports.getSecret = function () {
+  return secret;
+};
+
+exports.getId = function () {
+  return ident;
 };
